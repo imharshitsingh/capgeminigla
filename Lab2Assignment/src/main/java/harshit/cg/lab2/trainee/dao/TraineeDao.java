9ab2e63b14100843d29e93da2948c9f3884d8873
imharshitsingh <58332830+imharshitsingh@users.noreply.github.com>
2020-04-29 17:06:28 +0530

    Add files via upload

---
+++ Lab2Assignment/src/main/java/harshit/cg/lab2/trainee/dao/TraineeDao.java
@@ -0,0 +1,15 @@
+package harshit.cg.lab2.trainee.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import harshit.cg.lab2.trainee.entity.Trainee;
+
+@Transactional
+@Repository
+public interface TraineeDao extends JpaRepository<Trainee, Integer> {
+
+
+}
---
+++ Lab2Assignment/src/main/java/harshit/cg/lab2/trainee/dao/UserDao.java
@@ -0,0 +1,13 @@
+package harshit.cg.lab2.trainee.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import harshit.cg.lab2.trainee.entity.User;
+
+@Repository
+public interface UserDao extends JpaRepository<User, String>{
+	
+	public User findByUsernameAndPassword(String username, String password);
+
+}
